7d662eb07490f1b3cb49558128474bb86d70b87c
Rumi Parvez <openyhoolceo@gmail.com>
2026-09-18 18:00:28 +0600

    v-0.2

---
+++ src/app/daseboard/page.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Image from 'next/image';
+
+const DaseboardPage = () => {
+    return (
+        <div className="hover-3d">
+  {/* content */}
+  <figure className="max-w-100 rounded-2xl">
+    <Image src="https://img.daisyui.com/images/stock/creditcard.webp" height={500} width={700} alt="3D card" />
+  </figure>
+  {/* 8 empty divs needed for the 3D effect */}
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+</div>
+    );
+};
+
+export default DaseboardPage;
